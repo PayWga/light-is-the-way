7d70b0566ee7d9cefd37c36a6b26b3f74369d25f
LykRepchatiy <korsunskijartem@gmail.com>
2024-04-07 19:19:10 +0300

    add crosshair

--- Scripts/ShootController.gd
+++ Scripts/ShootController.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 const BULLET = preload("res://Prefabs/bullet.tscn")
+var crosshair = preload("res://Assets/Crosshairs/white_crosshair32.png")
+
+func _ready():
+	Input.set_custom_mouse_cursor(crosshair)
 
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
@@ -12,3 +16,4 @@ func shoot():
 	new_bullet.global_position = %ShootingPoint.global_position
 	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	get_tree().root.add_child(new_bullet)
+ 
